192e459c09d821cdb563a6f50389585527643339
Prasanth-kp-pro <57843565+Prasanth-kp-pro@users.noreply.github.com>
2023-08-26 13:19:26 +0530

    Update settings.py

--- lotty/settings.py
+++ lotty/settings.py
@@ -19,9 +19,7 @@ from urllib.parse import urlparse
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# [START gaestd_py_django_secret_config]
-env = environ.Env(DEBUG=(bool, False))
-env_file = os.path.join(BASE_DIR, ".env")
+
 
 if os.path.isfile(env_file):
     # Use a local secret file, if provided
