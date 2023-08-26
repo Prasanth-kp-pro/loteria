c369c4005db98d7a6007836ab6c459c520b2d411
Prasanth-kp-pro <57843565+Prasanth-kp-pro@users.noreply.github.com>
2023-08-26 13:31:56 +0530

    Update settings.py

--- lotty/settings.py
+++ lotty/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = '?c{jX<6Ip)Z=bpTf;sU*KBF$nu(.Qum*Cto+a=;dkB$C<v>@FeWg=nrJ4Y<6i$*'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
